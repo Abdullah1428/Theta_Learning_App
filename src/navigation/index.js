db174038f9492ff5d924ec5f3101673fc75d7423
Abdullah <abdullah>
2020-09-11 06:48:04 +0500

    boilerplate necessary libraries added

---
+++ src/navigation/index.js
@@ -0,0 +1,16 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import splashScreen from '../screens/splashScreen';
+
+const AppScreens = createStackNavigator(
+  {
+    splash: {
+      screen: splashScreen,
+    },
+  },
+  {
+    initialRouteName: 'splash',
+    headerMode: 'none',
+  },
+);
---
+++ src/screens/splashScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {StyleSheet, View, Text} from 'react-native';
+
+const splashScreen = ({navigation}) => {
+  return (
+    <View>
+      <Text>Hello from Splash Screen</Text>
+    </View>
+  );
+};
+
+export default splashScreen;
